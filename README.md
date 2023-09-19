747fde5e74622db1dec8abab540abc1963539d33
Hiul <hiufailau.41@gmail.com>
2023-09-19 23:43:11 +0200

    first test

--- README.md
+++
@@ -1,2 +0,0 @@
-# personal-task-management-app-ts
-## Using Typscript
---
+++ dist/index.377278e2.js
@@ -0,0 +1,8 @@
+function multiByTwo(number) {
+    if (typeof number === "number") return number * 2;
+    return "Please provide a valid number";
+}
+console.log(multiByTwo(4));
+console.log(multiByTwo(123));
+
+//# sourceMappingURL=index.377278e2.js.map
