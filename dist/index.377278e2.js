8bb528c5615702ee68c89d556e0f7d0da8076ae9
Hiul <hiufailau.41@gmail.com>
2023-09-22 15:54:24 +0200

    added boolean example

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -1,31 +1,12 @@
-let number = 100;
-number = 28.7;
-console.log(number);
-number = -300;
-console.log(number);
-number = Infinity;
-console.log(number);
-number = parseInt("12");
-console.log(number);
-number = Number("121");
-console.log(number);
-//Bigint
-let bigInt1 = BigInt(945845);
-let biInt2 = 123564564n;
-console.log(bigInt1);
-let a = BigInt(1234548);
-let b = 23456465n;
-console.log(b);
-let c = a - b;
-console.log(c);
-// let e:bigint = 24551.2n;
-// let f = Math.log(a);
-const safeInt = Number.MAX_SAFE_INTEGER;
-console.log(safeInt);
-const safeIntPlusOne = safeInt + 1;
-const safeIntPlusTwo = safeInt + 2;
-console.log(safeIntPlusOne);
-console.log(safeIntPlusTwo);
-console.log(safeIntPlusOne === safeIntPlusTwo);
+let firstname = "John";
+// firstname = 25;
+const number = 85;
+let numberTwo = 90;
+let finalResult;
+function addNumber(a, b) {
+    return a + b;
+}
+finalResult = addNumber(10, 15);
+console.log(finalResult);
 
 //# sourceMappingURL=index.377278e2.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,16 @@
+let firstname = "John";
 
+// firstname = 25;
 
-let number: number = 100;
+const number = 85;
 
-number = 28.7;
-console.log(number);
+let numberTwo = 90;
 
-number = -300;
-console.log(number);
+let finalResult: number;
 
-number = Infinity * 0.01;
-console.log(number);
+function addNumber(a: number, b: number) {
+  return a + b;
+}
 
-number = parseInt("12");
-console.log(number);
-
-number = Number("121");
-console.log(number);
-
-
-//Bigint
-let bigInt1 = BigInt(945845);
-
-let biInt2 = 123564564n;
-console.log(bigInt1);
-let a: bigint = BigInt(1234548);
-
-let b: bigint = 23456465n;
-console.log(b);
-
-let c: bigint = a - b;
-console.log(c);
-
-// let e:bigint = 24551.2n;
-// let f = Math.log(a);
-
-
-const safeInt = Number.MAX_SAFE_INTEGER;
-
-console.log(safeInt);
-
-const safeIntPlusOne = safeInt + 1;
-const safeIntPlusTwo = safeInt + 2;
-
-console.log(safeIntPlusOne);
-console.log(safeIntPlusTwo);
-
-console.log(safeIntPlusOne === safeIntPlusTwo);
+finalResult = addNumber(10, 15);
+console.log(finalResult);
