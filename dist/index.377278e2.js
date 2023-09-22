fa2db5fc619c81d9ec77c0111b30e31413dc9701
Hiul <hiufailau.41@gmail.com>
2023-09-22 14:52:21 +0200

    added boolean example

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -1,10 +1,31 @@
-let booleanValue = true;
-// Literal values
-booleanValue = false;
-// expression of true or false
-booleanValue = true;
-console.log(booleanValue);
-booleanValue = true;
-console.log(booleanValue);
+let number = 100;
+number = 28.7;
+console.log(number);
+number = -300;
+console.log(number);
+number = Infinity;
+console.log(number);
+number = parseInt("12");
+console.log(number);
+number = Number("121");
+console.log(number);
+//Bigint
+let bigInt1 = BigInt(945845);
+let biInt2 = 123564564n;
+console.log(bigInt1);
+let a = BigInt(1234548);
+let b = 23456465n;
+console.log(b);
+let c = a - b;
+console.log(c);
+// let e:bigint = 24551.2n;
+// let f = Math.log(a);
+const safeInt = Number.MAX_SAFE_INTEGER;
+console.log(safeInt);
+const safeIntPlusOne = safeInt + 1;
+const safeIntPlusTwo = safeInt + 2;
+console.log(safeIntPlusOne);
+console.log(safeIntPlusTwo);
+console.log(safeIntPlusOne === safeIntPlusTwo);
 
 //# sourceMappingURL=index.377278e2.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-let booleanValue: boolean = true;
 
-// Literal values
-booleanValue = false;
 
-// expression of true or false
-booleanValue = typeof "abc" == "string";
-console.log(booleanValue);
+let number: number = 100;
 
-booleanValue = 1 > 0;
+number = 28.7;
+console.log(number);
 
-console.log(booleanValue);
+number = -300;
+console.log(number);
 
+number = Infinity * 0.01;
+console.log(number);
+
+number = parseInt("12");
+console.log(number);
+
+number = Number("121");
+console.log(number);
+
+
+//Bigint
+let bigInt1 = BigInt(945845);
+
+let biInt2 = 123564564n;
+console.log(bigInt1);
+let a: bigint = BigInt(1234548);
+
+let b: bigint = 23456465n;
+console.log(b);
+
+let c: bigint = a - b;
+console.log(c);
+
+// let e:bigint = 24551.2n;
+// let f = Math.log(a);
+
+
+const safeInt = Number.MAX_SAFE_INTEGER;
+
+console.log(safeInt);
+
+const safeIntPlusOne = safeInt + 1;
+const safeIntPlusTwo = safeInt + 2;
+
+console.log(safeIntPlusOne);
+console.log(safeIntPlusTwo);
+
+console.log(safeIntPlusOne === safeIntPlusTwo);
