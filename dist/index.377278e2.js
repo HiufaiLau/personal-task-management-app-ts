3afc65b5489ea4b7e9ad5bdf01b877253983bc97
Hiul <hiufailau.41@gmail.com>
2023-09-22 16:28:29 +0200

    added obj type example

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -3,8 +3,9 @@ article = {
     author: "Stella",
     content: "Hello World",
     title: "My First Article",
-    date: new Date(),
+    date: new Date(date.toLocaleDateString()),
     wordCount: 100
 };
+console.log(article);
 
 //# sourceMappingURL=index.377278e2.js.map
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ article = {
   author: "Stella",
   content: "Hello World",
   title: "My First Article",
-  date: new Date(),
+  date: new Date(date.toLocaleDateString()),
   wordCount: 100,
 };
+
+console.log(article);
